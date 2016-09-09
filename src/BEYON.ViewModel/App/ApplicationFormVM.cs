91259de083f8d55198cc7c21168d125d4b1a82cb
zhangshuo <suredolphin4@126.com>
2016-09-09 15:08:52 +0800

    add userEmail fields

--- src/BEYON.ViewModel/App/ApplicationFormVM.cs
+++ src/BEYON.ViewModel/App/ApplicationFormVM.cs
@@ -44,6 +44,10 @@ namespace BEYON.ViewModel.App
         [StringLength(255)]
         public string AuditOpinion { get; set; }
 
+        [Display(Name = "用户Email")]
+        [StringLength(100)]
+        public string UserEmail { get; set; }
+
         [Display(Name = "报销合计")]
         public Double Summation { get; set; }
     }
