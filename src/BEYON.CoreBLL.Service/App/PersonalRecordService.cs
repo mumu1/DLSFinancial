296b54872f48bcc37b44a4e48d5af248b6064bf6
zhangshuo <suredolphin4@126.com>
2016-11-09 14:54:02 +0800

    1、修改对话框右上角关闭按钮数量；2、导入银行三个字段添加；

--- src/BEYON.CoreBLL.Service/App/PersonalRecordService.cs
+++ src/BEYON.CoreBLL.Service/App/PersonalRecordService.cs
@@ -191,6 +191,12 @@ namespace BEYON.CoreBLL.Service.App
                         PersonalRecord record = new PersonalRecord();
                         record.SerialNumber = serialNumber;
                         record.PaymentType = paymentType;
+                        if (paymentType.Equals("银行转账")) {
+                            record.AccountName = item[0];
+                            record.Bank = item[10];
+                            record.AccountNumber = item[11];
+                            record.BankDetailName = item[12];
+                        }
                         List<ImportFeedBack> errors = ValidatePersonalRecord(item,  num++, maps, ref record);
                         if(errors.Count > 0)
                         {
@@ -414,8 +420,8 @@ namespace BEYON.CoreBLL.Service.App
            // Regex regCerficateID = new Regex("^\\d{15}|\\d{18}$");
             //Regex regCerficateID = new Regex("^[1-9]\\d{5}[1-9]9\\d{4}3[0-1]\\d{4}$|^[1-9]\\d{5}[1-9]9\\d{4}[0-2][0-9]\\d{4}$|^[1-9]\\d{5}[1-9]9\\d{4}3[0-1]\\d{3}X$|^[1-9]\\d{5}[1-9]9\\d{4}[0-2][0-9]\\d{3}X$|^[1-9]\\d{5}\\d{4}3[0-1]\\d{4}$|^[1-9]\\d{5}\\d{4}[0-2][0-9]\\d{4}$|^[1-9]\\d{5}\\d{4}3[0-1]\\d{3}X$|^[1-9]\\d{5}\\d{4}[0-2][0-9]\\d{3}X$");
             //^[1-9]\d{5}[1-9]9\d{4}3[0-1]\d{4}$|^[1-9]\d{5}[1-9]9\d{4}[0-2][0-9]\d{4}$|^[1-9]\d{5}[1-9]9\d{4}3[0-1]\d{3}X$|^[1-9]\d{5}[1-9]9\d{4}[0-2][0-9]\d{3}X$|^[1-9]\d{5}\d{4}3[0-1]\d{4}$|^[1-9]\d{5}\d{4}[0-2][0-9]\d{4}$|^[1-9]\d{5}\d{4}3[0-1]\d{3}X$|^[1-9]\d{5}\d{4}[0-2][0-9]\d{3}X$
-            string Info = CheckCidInfo(personal.CertificateID);
-            if (!Info.Equals(personal.CertificateID))
+
+            if (CheckIDCard18(personal.CertificateID)== false && CheckIDCard15(personal.CertificateID) == false)
             {
                 feedBack.ExceptionContent.Add("第" + num + "行记录  证件号码格式有误！");
             }
@@ -497,41 +503,70 @@ namespace BEYON.CoreBLL.Service.App
 
         }
 
-        private string CheckCidInfo(string cid)
-{
-string[] aCity = new string[]{null,null,null,null,null,null,null,null,null,null,null,"北京","天津","河北","山西","古",null,null,null,null,null,"辽 宁","吉林","黑龙江",null,null,null,null,null,null,null,"上海","江苏","浙江","安微","福建","江西","山东",null,null,null,"河南","湖北","湖南","广东","广西","海南",null,null,null,"重庆","四川","贵州","云南","西藏",null,null,null,null,null,null,"陕西","甘肃","青海","宁夏","新疆",null,null,null,null,null,"台湾",null,null,null,null,null,null,null,null,null,"香港","澳门",null,null,null,null,null,null,null,null,"国外"};
-double iSum=0;
-string info="";
-System.Text.RegularExpressions.Regex rg = new System.Text.RegularExpressions.Regex(@"^\d{17}(\d|x)$");
-System.Text.RegularExpressions.Match mc = rg.Match(cid);
-if(!mc.Success)
-{
-return "";
-} 
-cid = cid.ToLower();
-cid = cid.Replace("x","a");
-if(aCity[int.Parse(cid.Substring(0,2))]==null)
-{
-return "非法地区";
-}
-try
-{
-DateTime.Parse(cid.Substring(6,4)+"-"+cid.Substring(10,2)+"-"+cid.Substring(12,2));
-}
-catch
-{
-return "非法生日";
-}
-for(int i=17;i>=0;i--)
-{ 
-iSum +=(System.Math.Pow(2,i)%11)*int.Parse(cid[17-i].ToString(),System.Globalization.NumberStyles.HexNumber);
+        private bool CheckIDCard18(string Id)
+        {
+            long n = 0;
+            if (long.TryParse(Id.Remove(17), out n) == false || n < Math.Pow(10, 16) || long.TryParse(Id.Replace('x', '0').Replace('X', '0'), out n) == false)
+            {
+                return false;//数字验证
+            }
 
-}
-if(iSum%11!=1)
-return("非法证号");
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
 
-return(aCity[int.Parse(cid.Substring(0,2))]+","+cid.Substring(6,4)+"-"+cid.Substring(10,2)+"-"+cid.Substring(12,2)+","+(int.Parse(cid.Substring(16,1))%2==1?"男":"女"));
+            if (address.IndexOf(Id.Remove(2)) == -1)
+            {
+                return false;//省份验证
+            }
+            string birth = Id.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+            DateTime time = new DateTime();
+            if (DateTime.TryParse(birth, out time) == false)
+            {
+                return false;//生日验证
+            }
+            string[] arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+            string[] Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+            char[] Ai = Id.Remove(17).ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+            }
+            int y = -1;
+            Math.DivRem(sum, 11, out y);
+            if (arrVarifyCode[y] != Id.Substring(17, 1).ToLower())
+            {
+                return false;//校验码验证
+            }
+            return true;//符合GB11643-1999标准
 
-}
+        }
+
+        private bool CheckIDCard15(string Id)
+        {
+            long n = 0;
+            if (long.TryParse(Id, out n) == false || n < Math.Pow(10, 14))
+            {
+                return false;//数字验证
+
+            }
+
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+
+            if (address.IndexOf(Id.Remove(2)) == -1)
+            {
+                return false;//省份验证
+            }
+
+            string birth = Id.Substring(6, 6).Insert(4, "-").Insert(2, "-");
+            DateTime time = new DateTime();
+
+            if (DateTime.TryParse(birth, out time) == false)
+            {
+                return false;//生日验证
+            }
+
+            return true;//符合15位身份证标准
+
+        }
     }
 }
